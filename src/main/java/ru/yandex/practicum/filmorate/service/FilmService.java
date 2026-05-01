aeed8480f37b94dfa89f4a979e376fb5b761e21a
Sergei <ssnefedov97@mail.ru>
2026-05-02 01:06:07 +0300

    редактирование согласно Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -18,7 +18,7 @@ public class FilmService {
 
     private final FilmStorage filmStorage;
     private final UserStorage userStorage;
-    private final static LocalDate FIRST_FILM = LocalDate.of(1895,12,28);
+    private static final LocalDate FIRST_FILM = LocalDate.of(1895,12,28);
 
     public  FilmService(FilmStorage filmStorage, UserStorage userStorage) {
         this.filmStorage = filmStorage;
